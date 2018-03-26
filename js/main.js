d7814a0daa94fecb909171c79a5bf1b34d2fdad8
Luke Mico <lukemico@me.com>
2018-03-27 00:37:31 +1100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,145 +1,127 @@
-// $(document).ready(function() {
-//   $("#loader loader-double").each(function() {
-//     animationHover(this, "fadeOut");
-//   });
-// });
 
-$(document).ready(function() {
-  $(function() {
-    var player = 1;
-    var table = $("table");
-    var message = $(".message");
-    var turn = $(".turn");
-    displayNextPlayer(turn, player);
 
-    $("td").click(function() {
-      td = $(this);
-      var state = getState(td);
-      if (!state) {
-        var pattern = definePatternForCurrentPlayer(player);
-        changeState(td, pattern);
-        if (checkIfPlayerWon(table, pattern)) {
-          window.alert(
-            "Player " + player + " has wins! DONUTS for everyone..."
-          );
-          // message.html('Player '+player+' has won.');
-          turn.html("");
-        } else {
-          player = setNextPlayer(player);
-          displayNextPlayer(turn, player);
+var turn = 'X';
+var score = {
+    'X': 0,
+    'O': 0
+};
+var gridValue = 0;
+
+function fnLoad() {
+    var select = document.getElementById("grid");
+    for (i = 3; i <= 100; i += 1) {
+        var option = document.createElement('option');
+        select.options[select.options.length] = new Option(i + ' X ' + i, i);
+    }
+
+    addEvent(document.getElementById("game"), "click", fnChoose);
+
+    fnNewGame();
+}
+
+function addEvent(element, eventName, callback) {
+
+    if (element.addEventListener) {
+        element.addEventListener(eventName, callback, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, callback);
+    }
+}
+
+function fnChoose(e) {
+    if (e.target && e.target.nodeName == "TD") {
+        var targetElement = document.getElementById(e.target.id);
+        var prevTurn;
+        if ((targetElement.className).indexOf("disabled") == -1) {
+            targetElement.innerHTML = turn;
+            targetElement.classList.add('disabled');
+            targetElement.classList.add(turn);
+            score[turn] += 1;
+            prevTurn = turn;
+            turn = turn === "X" ? "O" : "X";
+            if (fndecide(targetElement, prevTurn)) {
+                alert(prevTurn + ' has won the game');
+                fnNewGame();
+            } else if ((score['X'] + score['O']) == (gridValue * gridValue)) {
+                alert('Draw!');
+                fnNewGame();
+            }
         }
-      } else {
-        window.alert("Duh! This box is already checked.");
-        // message.html('This box is already checked.');
-      }
-    });
-
-    $(".reset").click(function() {
-      player = 1;
-      message.html("");
-      reset(table);
-      displayNextPlayer(turn, player);
-    });
-  });
-
-  function getState(td) {
-    if (td.hasClass("cross") || td.hasClass("circle")) {
-      return 1;
-    } else {
-      return 0;
     }
-  }
+}
 
-  function changeState(td, pattern) {
-    return td.addClass(pattern);
-  }
-
-  function definePatternForCurrentPlayer(player) {
-    if (player === 1) {
-      return "cross";
-    } else {
-      return "circle";
+function fndecide(targetElement, prevTurn) {
+    var UL = document.getElementById('game');
+    var elements, i, j, cnt;
+    if (score[prevTurn] >= gridValue) {
+        var classes = targetElement.className.split(/\s+/);
+        for (i = 0; i < classes.length; i += 1) {
+            cnt = 0;
+            if (classes[i].indexOf('row') !== -1 || classes[i].indexOf('col') !== -1 || classes[i].indexOf('dia') !== -1) {
+                elements = UL.getElementsByClassName(classes[i]);
+                for (j = 0; j < elements.length; j += 1) {
+                    if (elements[j].innerHTML == prevTurn) {
+                        cnt += 1;
+                    }
+                }
+                if (cnt == gridValue) {
+                    return true;
+                }
+            }
+        }
     }
-  }
+    return false;
+}
 
-  function setNextPlayer(player) {
-    if (player === 1) {
-      return (player = 2);
-    } else {
-      return (player = 1);
+function fnNewGame() {
+    var gameUL = document.getElementById("game");
+    if (gameUL.innerHTML !== '') {
+        gameUL.innerHTML = null;
+        score = {
+            'X': 0,
+            'O': 0
+        };
+        turn = 'X';
+        gridValue = 0;
     }
-  }
+    var select = document.getElementById("grid");
+    gridValue = select.options[select.selectedIndex].value;
+    var i, j, li, k = 0,
+        classLists;
+    var gridAdd = +gridValue + 1;
 
-  function displayNextPlayer(turn, player) {
-    turn.html("It's your turn, player: " + player);
-  }
+    for (i = 1; i <= gridValue; i += 1) {
+        tr = document.createElement('tr');
+        for (j = 1; j <= gridValue; j += 1) {
+            k += 1;
+            li = document.createElement('td');
+            li.setAttribute("id", 'li' + k);
 
-  function checkIfPlayerWon(table, pattern) {
-    var won = 0;
-    if (
-      table.find(".item1").hasClass(pattern) &&
-      table.find(".item2").hasClass(pattern) &&
-      table.find(".item3").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item1").hasClass(pattern) &&
-      table.find(".item4").hasClass(pattern) &&
-      table.find(".item7").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item1").hasClass(pattern) &&
-      table.find(".item5").hasClass(pattern) &&
-      table.find(".item9").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item4").hasClass(pattern) &&
-      table.find(".item5").hasClass(pattern) &&
-      table.find(".item6").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item7").hasClass(pattern) &&
-      table.find(".item8").hasClass(pattern) &&
-      table.find(".item9").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item2").hasClass(pattern) &&
-      table.find(".item5").hasClass(pattern) &&
-      table.find(".item8").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item3").hasClass(pattern) &&
-      table.find(".item6").hasClass(pattern) &&
-      table.find(".item9").hasClass(pattern)
-    ) {
-      won = 1;
-    } else if (
-      table.find(".item3").hasClass(pattern) &&
-      table.find(".item5").hasClass(pattern) &&
-      table.find(".item7").hasClass(pattern)
-    ) {
-      won = 1;
+            classLists = 'td row' + i + ' col' + j;
+
+            if (i === j) {
+                classLists = 'td row' + i + ' col' + j + ' dia0';
+            }
+
+            if ((i + j) === gridAdd) {
+                classLists = 'td row' + i + ' col' + j + ' dia1';
+            }
+
+            if (!isEven(gridValue) && (Math.round(gridValue / 2) === i && Math.round(gridValue / 2) === j))
+                classLists = 'td row' + i + ' col' + j + ' dia0 dia1';
+
+            li.className = classLists;
+            tr.appendChild(li);
+
+        }
+        gameUL.appendChild(tr);
     }
-    return won;
-  }
+}
 
-  function reset(table) {
-    console.log("jkhfjhf");
-    table.find("td").each(function() {
-      $(this).removeClass("cross").removeClass("circle");
-    });
-  }
-});
 
-var h1 = setInterval("doSomething()", 5000);
-
-$('.first').addClass('animated fadeInUp');
-
-setTimeout(function () {
-    $('.first').show().addClass('animated fadeInUp');}, 1500
-);
+function isEven(value) {
+    if (value % 2 == 0)
+        return true;
+    else
+        return false;
+}
